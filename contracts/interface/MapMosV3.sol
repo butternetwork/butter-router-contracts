c472416f578991d44e2947af52c44c50629e61df
Jimmie3 <lcx3452@gmail.com>
2023-02-06 11:17:11 +0800

    add event SwapAndBridge

--- contracts/interface/MapMosV3.sol
+++ contracts/interface/MapMosV3.sol
@@ -4,6 +4,18 @@ pragma solidity ^0.8.0;
 
 interface MapMosV3 {
 
+    struct SwapParam {
+        uint256 amountIn;
+        uint256 minAmountOut;
+        bytes path; //evm, or tokenIn'X'tokenOut on near
+        uint64 routerIndex; // pool id on near or router index on evm
+    }
+
+    struct SwapData {
+        SwapParam[] swapParams;
+        bytes targetToken;
+        address mapTargetToken;
+    }
 
     function swapOutToken(
         address _initiatorAddress,
