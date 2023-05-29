ddc508230c1ff6d2df2850fae408f621d2be949c
Jimmie3 <lcx3452@gmail.com>
2023-05-29 18:10:37 +0800

    config  --optimize

--- hardhat.config.js
+++ hardhat.config.js
@@ -24,19 +24,20 @@ module.exports = {
     hardhat: {
       chainId:1,
       initialBaseFeePerGas: 0,
-      // forking: {
-      //   url: "https://eth-mainnet.alchemyapi.io/v2/" + process.env.ALCHEMY_KEY,
-      //   blockNumber: 16930373        //15986531
-      // }
+    },
+    Eth: {
+      url: `https://mainnet.infura.io/v3/` + process.env.INFURA_KEY,
+      chainId : 1,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    Map : {
+      chainId: 22776,
+      url:"https://rpc.maplabs.io",
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
     Matic: {
       url: `https://polygon.llamarpc.com`,
       chainId : 137,
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-    MaticTest: {
-      url: `https://polygon-mumbai.blockpi.network/v1/rpc/public`,
-      chainId : 80001,
       accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
     Avalanche: {
@@ -45,58 +46,18 @@ module.exports = {
       accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
     Bsc: {
-      url: `https://bsc-dataseed1.binance.org/`,
+      url: `https://rpc.ankr.com/bsc`,
       chainId : 56,
       accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
-    BscTest: {
-      url: "https://bsc-testnet.public.blastapi.io",
-      chainId : 97,
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-    Map : {
-      chainId: 22776,
-      url:"https://rpc.maplabs.io",
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-    Makalu: {
-      chainId: 212,
-      url:"https://testnet-rpc.maplabs.io",
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-    Eth: {
-      url: `https://mainnet.infura.io/v3/` + process.env.INFURA_KEY,
-      chainId : 1,
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-    Goerli: {
-      url: "https://rpc.ankr.com/eth_goerli",
-      chainId : 5,
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-    Klay: {
+    klaytn: {
       url: `https://public-node-api.klaytnapi.com/v1/cypress`,
       chainId : 8217,
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-    KlayTest: {
-      url: `https://api.baobab.klaytn.net:8651/`,
-      chainId : 1001,
       accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
     Arbitrum: {
       url: `https://1rpc.io/arb`,
       chainId : 42161,
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-    Op: {
-      url: `https://1rpc.io/op`,
-      chainId : 10,
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-    Avax: {
-      url: `https://rpc.ankr.com/avalanche`,
-      chainId : 43114,
       accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
     Fantom: {
@@ -112,6 +73,119 @@ module.exports = {
     Aurora: {
       url: `https://mainnet.aurora.dev`,
       chainId : 1313161554,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    Astar: {
+      url: `https://rpc.astar.network:8545/`,
+      chainId : 592,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    Boba: {
+      url: `https://mainnet.boba.network`,
+      chainId : 288,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    Celo: {
+      url: `https://rpc.ankr.com/celo`,
+      chainId : 42220,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    Cronos: {
+      url: `https://cronos-evm.publicnode.com`,
+      chainId : 25,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    Dfk: {
+      url: `https://dfkchain.api.onfinality.io/public`,
+      chainId : 53935,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    Ethpow: {
+      url: `https://mainnet.ethereumpow.org`,
+      chainId : 10001,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    Harmony: {
+      url: `https://rpc.ankr.com/harmony`,
+      chainId : 1666600000,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    Kava: {
+      url: `https://evm2.kava.io`,
+      chainId : 2222,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    Metis: {
+      url: `https://andromeda.metis.io/?owner=1088`,
+      chainId : 1088,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    Moonbeam: {
+      url: `https://rpc.ankr.com/moonbeam`,
+      chainId : 1284,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    moonriver: {
+      url: `https://rpc.api.moonriver.moonbeam.network`,
+      chainId : 1285,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    Oasis: {
+      url: `https://emerald.oasis.dev`,
+      chainId : 42262,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    Syscoin: {
+      url: `https://rpc.syscoin.org`,
+      chainId : 57,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    Telos: {
+      url: `https://rpc1.eu.telos.net/evm`,
+      chainId : 40,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    Velas: {
+      url: `https://evmexplorer.velas.com/rpc`,
+      chainId : 106,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    Zksync: {
+      url: `https://mainnet.era.zksync.io`,
+      chainId : 324,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    Optimism: {
+      url: `https://1rpc.io/op`,
+      chainId : 10,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+
+
+    
+    MaticTest: {
+      url: `https://polygon-mumbai.blockpi.network/v1/rpc/public`,
+      chainId : 80001,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    BscTest: {
+      url: "https://endpoints.omniatech.io/v1/bsc/testnet/public",
+      chainId : 97,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    Makalu: {
+      chainId: 212,
+      url:"https://testnet-rpc.maplabs.io",
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    Goerli: {
+      url: "https://rpc.ankr.com/eth_goerli",
+      chainId : 5,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    KlayTest: {
+      url: `https://api.baobab.klaytn.net:8651/`,
+      chainId : 1001,
       accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
   }
--- tasks/config.js
+++ tasks/config.js
@@ -9,7 +9,14 @@ let config = new Map(
             feeRate:"3000",//denominator is 1000000
             fixedFee:"100000000"
                 },
-            excutors:[]
+            excutors:[
+                "0x0000000022D53366457F9d5E68Ec105046FC4383",//curve
+                "0x1111111254eeb25477b68fb85ed929f73a960582",//1inch
+                "0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F",//sushi 
+                "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",//univ2
+                "0xE592427A0AEce92De3Edee1F18E0157C05861564",//univ3
+                "0xB4B0ea46Fe0E9e8EAB4aFb765b527739F2718671",//verse 
+            ]
         }],
 
         ["Avalanche",{
@@ -21,6 +28,7 @@ let config = new Map(
             fixedFee:"100000000"
                 },
             excutors:[
+                "0x0000000022D53366457F9d5E68Ec105046FC4383",//curve 
                 "0x60aE616a2155Ee3d9A68541Ba4544862310933d4",//joe
                 "0x1111111254eeb25477b68fb85ed929f73a960582",//1inch 
                 "0xE54Ca86531e17Ef3616d22Ca28b0D458b6C89106",//pangolin 
@@ -61,16 +69,335 @@ let config = new Map(
             ]
         }],
 
-        ["Map",{
-            wToken:"",
-            mos:"",
+        ["Arbitrum",{
+            wToken:"0x82aF49447D8a07e3bd95BD0d56f35241523fBab1",
+            mos:"0x82aF49447D8a07e3bd95BD0d56f35241523fBab1",
             fee:{
             receiver:"",
             feeRate:"3000",//denominator is 1000000
             fixedFee:"100000000"
                 },
             excutors:[
+                "0x0000000022D53366457F9d5E68Ec105046FC4383",//curve	
+                "0x1111111254eeb25477b68fb85ed929f73a960582",//1inch
+                "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506",//sushi 
+                "0xE592427A0AEce92De3Edee1F18E0157C05861564"//univ3
+            ]
+        }],
 
+        ["Optimism",{
+            wToken:"0x4200000000000000000000000000000000000006",
+            mos:"0x4200000000000000000000000000000000000006",
+            fee:{
+            receiver:"",
+            feeRate:"3000",//denominator is 1000000
+            fixedFee:"100000000"
+                },
+            excutors:[
+                "0x0000000022D53366457F9d5E68Ec105046FC4383",//curve	
+                "0x1111111254eeb25477b68fb85ed929f73a960582",//1inch
+            ]
+        }],
+
+        ["Zksync",{
+            wToken:"0x5AEa5775959fBC2557Cc8789bC1bf90A239D9a91",
+            mos:"0x5AEa5775959fBC2557Cc8789bC1bf90A239D9a91",
+            fee:{
+            receiver:"",
+            feeRate:"3000",//denominator is 1000000
+            fixedFee:"100000000"
+                },
+            excutors:[
+                "0x8B791913eB07C32779a16750e3868aA8495F5964",//mute 	
+                "0x1111111254eeb25477b68fb85ed929f73a960582",//1inch
+                "0x2da10A1e27bF85cEdD8FFb1AbBe97e53391C0295",//sync-swap 	
+            ]
+        }],
+
+
+        ["Aurora",{
+            wToken:"0xC9BdeEd33CD01541e1eeD10f90519d2C06Fe3feB",
+            mos:"0xC9BdeEd33CD01541e1eeD10f90519d2C06Fe3feB",
+            fee:{
+            receiver:"",
+            feeRate:"3000",//denominator is 1000000
+            fixedFee:"100000000"
+                },
+            excutors:[	
+                "0x1111111254eeb25477b68fb85ed929f73a960582",//1inch
+                "0x2CB45Edb4517d5947aFdE3BEAbF95A582506858B",//trisolaris 
+                "0xa3a1eF5Ae6561572023363862e238aFA84C72ef5"//wanna 
+            ]
+        }],
+
+        ["Boba",{
+            wToken:"0xDeadDeAddeAddEAddeadDEaDDEAdDeaDDeAD0000",
+            mos:"0xDeadDeAddeAddEAddeadDEaDDEAdDeaDDeAD0000",
+            fee:{
+            receiver:"",
+            feeRate:"3000",//denominator is 1000000
+            fixedFee:"100000000"
+                },
+            excutors:[	
+                "0x17C83E2B96ACfb5190d63F5E46d93c107eC0b514",//oolong 
+            ]
+        }],
+
+        ["Celo",{
+            wToken:"0x471EcE3750Da237f93B8E339c536989b8978a438",
+            mos:"0x471EcE3750Da237f93B8E339c536989b8978a438",
+            fee:{
+            receiver:"",
+            feeRate:"3000",//denominator is 1000000
+            fixedFee:"100000000"
+                },
+            excutors:[	
+                "0x0000000022D53366457F9d5E68Ec105046FC4383",//curve  
+            ]
+        }],
+
+        ["Cronos",{
+            wToken:"0x5C7F8A570d578ED84E63fdFA7b1eE72dEae1AE23",
+            mos:"0x5C7F8A570d578ED84E63fdFA7b1eE72dEae1AE23",
+            fee:{
+            receiver:"",
+            feeRate:"3000",//denominator is 1000000
+            fixedFee:"100000000"
+                },
+            excutors:[	
+                "0x6c3A0E2E78848274B7E3346b8Ef8a4cBB2fEE2a9",//cro-swap  
+                "0xeC0A7a0C2439E8Cb67b992b12ecd020Ea943c7Be",//crodex
+                "0xcd7d16fb918511bf7269ec4f48d61d79fb26f918",//crona-swap 
+            ]
+        }],
+
+        ["Dfk",{
+            wToken:"0xCCb93dABD71c8Dad03Fc4CE5559dC3D89F67a260",
+            mos:"0xCCb93dABD71c8Dad03Fc4CE5559dC3D89F67a260",
+            fee:{
+            receiver:"",
+            feeRate:"3000",//denominator is 1000000
+            fixedFee:"100000000"
+                },
+            excutors:[	
+                "0x3C351E1afdd1b1BC44e931E12D4E05D6125eaeCa",//trader-dfk 
+            ]
+        }],
+
+        ["Ethpow",{
+            wToken:"0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+            mos:"0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+            fee:{
+            receiver:"",
+            feeRate:"3000",//denominator is 1000000
+            fixedFee:"100000000"
+                },
+            excutors:[	
+                "0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F",//sushi 
+                "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",//univ2 
+                "0xE592427A0AEce92De3Edee1F18E0157C05861564",//univ3 
+            ]
+        }],
+
+        ["Fantom",{
+            wToken:"0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83",
+            mos:"0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83",
+            fee:{
+            receiver:"",
+            feeRate:"3000",//denominator is 1000000
+            fixedFee:"100000000"
+                },
+            excutors:[	
+                "0x0000000022D53366457F9d5E68Ec105046FC4383",//curve 
+                "0x1111111254eeb25477b68fb85ed929f73a960582",//1inch 
+                "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506",//sushi 
+                "0x6b3d631B87FE27aF29efeC61d2ab8CE4d621cCBF",//soul 
+                "0x16327E3FbDaCA3bcF7E38F5Af2599D2DDc33aE52",//spirit  
+                "0xF491e7B69E4244ad4002BC14e878a34207E38c29",//spooky 
+            ]
+        }],
+
+        ["Gnosis",{
+            wToken:"0xe91D153E0b41518A2Ce8Dd3D7944Fa863463a97d",
+            mos:"0xe91D153E0b41518A2Ce8Dd3D7944Fa863463a97d",
+            fee:{
+            receiver:"",
+            feeRate:"3000",//denominator is 1000000
+            fixedFee:"100000000"
+                },
+            excutors:[	
+                "0x0000000022D53366457F9d5E68Ec105046FC4383",//curve
+                "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506",//sushi 
+                "0x1C232F01118CB8B424793ae03F870aa7D0ac7f77",//Honeyswap
+            ]
+        }],
+
+
+        ["Harmony",{
+            wToken:"0xcF664087a5bB0237a0BAd6742852ec6c8d69A27a",
+            mos:"0xcF664087a5bB0237a0BAd6742852ec6c8d69A27a",
+            fee:{
+            receiver:"",
+            feeRate:"3000",//denominator is 1000000
+            fixedFee:"100000000"
+                },
+            excutors:[	
+                "0x24ad62502d1C652Cc7684081169D04896aC20f30",//trader 
+                "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506",//sushi 
+                "0xf012702a5f0e54015362cBCA26a26fc90AA832a3",//viper 
+            ]
+        }],
+
+        ["Kava",{
+            wToken:"0xc86c7C0eFbd6A49B35E8714C5f59D99De09A225b",
+            mos:"0xc86c7C0eFbd6A49B35E8714C5f59D99De09A225b",
+            fee:{
+            receiver:"",
+            feeRate:"3000",//denominator is 1000000
+            fixedFee:"100000000"
+                },
+            excutors:[	
+                "0x0000000022D53366457F9d5E68Ec105046FC4383",//curve 
+                "0x7a2c1D96C76B6EB62241df4d2fAEb9F0D3D59E10",//elk  
+                "0xEa3CB4Ba9d1fD6fb19Df1380958d5649bD3e7C50",//jupiter 
+                "0x8a340F39A468C2FcBFFf2122446a9A0745A313Ad",//photon 
+                "0x4310ed61E7E4fd50C2b44C92725C087abeB632a2",//surfdex 
+            ]
+        }],
+
+        ["klaytn",{
+            wToken:"0xe4f05A66Ec68B54A58B17c22107b02e0232cC817",
+            mos:"0xe4f05A66Ec68B54A58B17c22107b02e0232cC817",
+            fee:{
+            receiver:"",
+            feeRate:"3000",//denominator is 1000000
+            fixedFee:"100000000"
+                },
+            excutors:[	
+                "0xEf71750C100f7918d6Ded239Ff1CF09E81dEA92D",//claim 
+            ]
+        }],
+
+        ["Metis",{
+            wToken:"0xdeaddeaddeaddeaddeaddeaddeaddeaddead0000",
+            mos:"0xdeaddeaddeaddeaddeaddeaddeaddeaddead0000",
+            fee:{
+            receiver:"",
+            feeRate:"3000",//denominator is 1000000
+            fixedFee:"100000000"
+                },
+            excutors:[	
+                "0x1E876cCe41B7b844FDe09E38Fa1cf00f213bFf56",//net-swap 
+            ]
+        }],
+
+        ["Moonbeam",{
+            wToken:"0xAcc15dC74880C9944775448304B263D191c6077F",
+            mos:"0xAcc15dC74880C9944775448304B263D191c6077F",
+            fee:{
+            receiver:"",
+            feeRate:"3000",//denominator is 1000000
+            fixedFee:"100000000"
+                },
+            excutors:[	
+                "0x0000000022D53366457F9d5E68Ec105046FC4383",//curve  
+            ]
+        }],
+
+        ["Moonriver",{
+            wToken:"0xf50225a84382c74CbdeA10b0c176f71fc3DE0C4d",
+            mos:"0xf50225a84382c74CbdeA10b0c176f71fc3DE0C4d",
+            fee:{
+            receiver:"",
+            feeRate:"3000",//denominator is 1000000
+            fixedFee:"100000000"
+                },
+            excutors:[	
+                "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506",//sushi   
+                "0xAA30eF758139ae4a7f798112902Bf6d65612045f",//solarbeam   
+            ]
+        }],
+
+        
+        ["Oasis",{
+            wToken:"0x21C718C22D52d0F3a789b752D4c2fD5908a8A733",
+            mos:"0x21C718C22D52d0F3a789b752D4c2fD5908a8A733",
+            fee:{
+            receiver:"",
+            feeRate:"3000",//denominator is 1000000
+            fixedFee:"100000000"
+                },
+            excutors:[	
+                "0x250d48C5E78f1E85F7AB07FEC61E93ba703aE668",//yuzu-swap   
+            ]
+        }],
+
+        ["Syscoin",{
+            wToken:"0xd3e822f3ef011ca5f17d82c956d952d8d7c3a1bb",
+            mos:"0xd3e822f3ef011ca5f17d82c956d952d8d7c3a1bb",
+            fee:{
+            receiver:"",
+            feeRate:"3000",//denominator is 1000000
+            fixedFee:"100000000"
+                },
+            excutors:[	
+                "0x017dAd2578372CAEE5c6CddfE35eEDB3728544C4",//pegasys    
+            ]
+        }],
+
+        ["Telos",{
+            wToken:"0xD102cE6A4dB07D247fcc28F366A623Df0938CA9E",
+            mos:"0xD102cE6A4dB07D247fcc28F366A623Df0938CA9E",
+            fee:{
+            receiver:"",
+            feeRate:"3000",//denominator is 1000000
+            fixedFee:"100000000"
+                },
+            excutors:[	
+                "0xb9667cf9a495a123b0c43b924f6c2244f42817be",//ape-swap    
+                "0xF9678db1CE83f6f51E5df348E2Cc842Ca51EfEc1",//omnidex	
+                "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506",//sushi 
+                "0xb9239af0697c8efb42cba3568424b06753c6da71",//zappy 
+            ]
+        }],
+
+        ["Velas",{
+            wToken:"0xc579D1f3CF86749E05CD06f7ADe17856c2CE3126",
+            mos:"0xc579D1f3CF86749E05CD06f7ADe17856c2CE3126",
+            fee:{
+            receiver:"",
+            feeRate:"3000",//denominator is 1000000
+            fixedFee:"100000000"
+                },
+            excutors:[	
+                "0x3328cd3a9A295cd00fBb1d71Bf097e002B4614ad",//astro     
+                "0x3D1c58B6d4501E34DF37Cf0f664A58059a188F00",//wagyu 	
+            ]
+        }],
+
+        ["Astar",{
+            wToken:"0xaeaaf0e2c81af264101b9129c00f4440ccf0f720",
+            mos:"0xaeaaf0e2c81af264101b9129c00f4440ccf0f720",
+            fee:{
+            receiver:"",
+            feeRate:"3000",//denominator is 1000000
+            fixedFee:"100000000"
+                },
+            excutors:[
+             "0xE915D2393a08a00c5A463053edD31bAe2199b9e7",//arts-swap
+            ]
+        }],
+
+        ["Map",{
+            wToken:"0x13cb04d4a5dfb6398fc5ab005a6c84337256ee23",
+            mos:"0x630105189c7114667a7179Aa57f07647a5f42B7F",
+            fee:{
+            receiver:"",
+            feeRate:"3000",//denominator is 1000000
+            fixedFee:"100000000"
+                },
+            excutors:[
+            "0x0bce9e0ebd4fd4d6562495af45c4aaa0c1f7f3d7",//hiveswap
             ]
         }],
 
