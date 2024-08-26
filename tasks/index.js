e00ea27169a00d9d3571d3b0b50b15a9aaa43499
Jimmie3 <lcx3452@gmail.com>
2024-08-26 17:27:31 +0800

    add receiver contract

--- tasks/index.js
+++ tasks/index.js
@@ -2,6 +2,7 @@ let { task } = require("hardhat/config");
 require("./subs/IntegratorManager.js");
 task("routerV2", "deploy butter router V2 contract and set", require("./subs/routerV2.js"));
 task("routerV3", "deploy butter router V3 contract and set", require("./subs/routerV3.js"));
+task("receiver", "deploy receiver contract and set", require("./subs/receiver.js"));
 task("deploySwapAdapter", "deploy swapAdapter contract", require("./subs/swapAdapter.js"));
 task("deployFeeReceiver", "deploy feeReceiver", require("./subs/deployFeeReceiver.js"))
     .addParam("payees", "payees address array")
